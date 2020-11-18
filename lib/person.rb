053157756de6acfe06ca2868af2fedba66eb3ed2
clairedeboer <claire.deboer@gmail.com>
2020-11-18 11:55:49 -0500

    initialization method first commit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,19 @@
-# your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :happiness, :hygiene, :bank_account
+
+  PEOPLE = []
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    # @happiness=happiness.clamp(0, 10)
+    @hygiene = 8
+  end
+
+  def self.all
+    PEOPLE
+  end
+
+end# your code goes here
